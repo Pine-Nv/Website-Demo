46ba9a5fd1e5d51bd9f3f79f1e90a5d4aaa6efa6
Harsh Vishwakarma <harshvish45@gmail.com>
2026-09-25 22:11:13 +0530

    web demo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import CategorySection from "../components/CategorySection";
 import FeaturedRecipes from "../components/FeaturedRecipes";
